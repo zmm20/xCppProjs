0684182e45313fd0c9982495985d1934cf2ebda9
周滿滿 <zhoumanman888@126.com>
2017-07-17 17:06:36 +0800

    test podofo

---
+++ helloPoDoFo/00test.cpp
@@ -0,0 +1,21 @@
+#define MAIN
+#ifdef MAIN
+
+#include <iostream>
+#include <podofo/podofo.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "test 1" << endl;
+    {
+        cout << "hello" << endl;
+    }
+    cout << endl;
+
+
+    return EXIT_SUCCESS;
+}
+
+#endif
