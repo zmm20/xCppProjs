59d09c35bfefec2d433127b084d7708bf35ea95a
周滿滿 <zhoumanman888@126.com>
2017-07-17 18:09:07 +0800

    aaa

--- helloPoDoFo/00test.cpp
+++ helloPoDoFo/00test.cpp
@@ -3,14 +3,48 @@
 
 #include <iostream>
 #include <podofo/podofo.h>
+#include <string>
 
 using namespace std;
+using namespace PoDoFo;
+
+int count_pages(const char* pszFilename, const bool & bShortFormat)
+{
+    PdfMemDocument document;
+    document.Load(pszFilename);
+    int nPages = document.GetPageCount();
+
+    if (bShortFormat)
+        printf("%i\n", nPages);
+    else
+        printf("%s:\t%i\n", pszFilename, nPages);
+
+    return nPages;
+}
+
 
 int main()
 {
+    cout << "-----00test------------" << endl;
+
+    //PdfError::EnableDebug(false);
+    const std::string pdfPath(R"(.\test\test.pdf)");
+
     cout << "test 1" << endl;
-    {
-        cout << "hello" << endl;
+    {        
+        try {
+
+            PdfMemDocument document;
+            document.Load(pdfPath.c_str());
+            int nPages = document.GetPageCount();
+
+            cout << "pdfÒ³Êý:" << nPages << endl;
+        }
+        catch (PdfError & e) {
+            fprintf(stderr, "Error: An error %i ocurred during counting pages in the pdf file.\n", e.GetError());
+            e.PrintErrorMsg();
+            return e.GetError();
+        }
     }
     cout << endl;
 
