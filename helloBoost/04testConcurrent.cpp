c18b0c6b52e5572d42696ea5cff7c8b5e4670f0e
周滿滿 <zhoumanman888@126.com>
2017-07-20 18:13:02 +0800

    共享锁

--- helloBoost/04testConcurrent.cpp
+++ helloBoost/04testConcurrent.cpp
@@ -1,12 +1,15 @@
-//#define MAIN
+#define MAIN
 #ifdef MAIN
 
 #include <iostream>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
+#include <boost/thread/thread_pool.hpp>
 #include <string>
+#include <functional>
 #include "MyBuffer.h"
+#include "RWData.h"
 using namespace std;
 using namespace boost;
 
@@ -31,6 +34,30 @@ void consumer(int n)
     }
 }
 
+
+// test 2
+// ¹²ÏíËø
+void writer(RWData& d)
+{
+    for (int i = 0; i < 20; ++i)
+    {
+        this_thread::sleep(posix_time::millisec(10));
+        d.write();
+    }
+}
+
+void reader(RWData& d, mutex& io_mu)
+{
+    int x;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        this_thread::sleep(posix_time::millisec(5));
+        d.read(x);
+        mutex::scoped_lock lock(io_mu);
+        std::cout << "reader:" << x << std::endl;
+    }
+}
 int main()
 {
     cout << "test 1" << endl;
@@ -50,6 +77,19 @@ int main()
 
     cout << "test 2" << endl;
     {
+        // http://blog.csdn.net/hh794362661/article/details/46540221
+        RWData d;
+        thread_group pool;
+        mutex io_mu;
+
+        pool.create_thread(bind(reader, boost::ref(d), boost::ref(io_mu)));
+        pool.create_thread(bind(reader, boost::ref(d), boost::ref(io_mu)));
+        pool.create_thread(bind(reader, boost::ref(d), boost::ref(io_mu)));
+        pool.create_thread(bind(reader, boost::ref(d), boost::ref(io_mu)));
+        pool.create_thread(bind(writer, boost::ref(d)));
+        pool.create_thread(bind(writer, boost::ref(d)));
+
+        pool.join_all();
     }
     cout << endl;
 }
