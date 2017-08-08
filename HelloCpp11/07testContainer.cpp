7e1e1173b6ef198cc136a8ebd892e7ab25439175
周滿滿 <zhoumanman888@126.com>
2017-08-08 09:30:43 +0800

    aaa

--- HelloCpp11/07testContainer.cpp
+++ HelloCpp11/07testContainer.cpp
@@ -18,6 +18,7 @@
 using namespace std;
 template <typename T>
 void PRINT_ELEMENTS(const T& coll, const std::string& optstr = "");
+void printLists(const string& s, const forward_list<int>& l1, const forward_list<int>& l2);
 int main()
 {
     cout << "test 1" << endl;
@@ -168,6 +169,35 @@ int main()
         list.insert_after(posBefore, 42);
         PRINT_ELEMENTS(list);
 
+
+        // create two forward lists
+        forward_list<int> list1 = { 1, 2, 3, 4 };
+        forward_list<int> list2 = { 77, 88, 99 };
+        printLists("initial:", list1, list2);
+        // insert six new element at the beginning of list2
+        list2.insert_after(list2.before_begin(), 99);
+        list2.push_front(10);
+        list2.insert_after(list2.before_begin(), { 10,11,12,13 });
+        printLists("6 new elems:", list1, list2);
+        // insert all elements of list2 at the beginning of list1
+        list1.insert_after(list1.before_begin(),
+            list2.begin(), list2.end());
+        printLists("list2 into list1:", list1, list2);
+        // delete second element and elements after element with value 99
+        list2.erase_after(list2.begin());
+        list2.erase_after(find(list2.begin(), list2.end(),
+            99),
+            list2.end());
+        printLists("delete 2nd and after 99:", list1, list2);
+        // sort list1, assign it to list2, and remove duplicates
+        list1.sort();
+        list2 = list1;
+        list2.unique();
+        printLists("sorted and unique:", list1, list2);
+        // merge both sorted lists into list1
+        list1.merge(list2);
+        printLists("merged:", list1, list2);
+
     }
     cout << endl;
 
@@ -188,5 +218,16 @@ inline void PRINT_ELEMENTS(const T& coll, const std::string& optstr)
         std::cout << elem << " ";
     }
     std::cout << std::endl;
+}
+
+void printLists(const string& s, const forward_list<int>& l1,
+    const forward_list<int>& l2)
+{
+    cout << s << endl;
+    cout << " list1: ";
+    copy(l1.cbegin(), l1.cend(), ostream_iterator<int>(cout, " "));
+    cout << endl << " list2: ";
+    copy(l2.cbegin(), l2.cend(), ostream_iterator<int>(cout, " "));
+    cout << endl;
 }
 #endif
